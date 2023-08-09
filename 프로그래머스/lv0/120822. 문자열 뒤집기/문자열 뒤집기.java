9365af435bb0472a87631c79807d0c82255d9f1a
daon <126737580+shin-daon@users.noreply.github.com>
2023-08-09 21:13:15 +0900

    [level 0] Title: 문자열 뒤집기, Time: 7.43 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string) {
+        
+        StringBuffer sb = new StringBuffer();
+        sb.append(my_string);
+        sb.reverse();
+        
+        return sb+"";
+    }
+}
