0370d4e10a2f32a3639a2a3010339e5718805acf
daon <126737580+shin-daon@users.noreply.github.com>
2023-08-10 18:44:34 +0900

    [level 0] Title: 모음 제거, Time: 0.18 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        String[] vowel = {"a", "e", "i", "o", "u"};
+        
+        for(int i=0; i<vowel.length; i++){
+            if(my_string.contains(vowel[i])){
+                my_string = my_string.replaceAll(vowel[i], "");
+            }
+        }
+        
+        return my_string;
+    }
+}
