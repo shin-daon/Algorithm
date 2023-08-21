d3d663593bd0fb51e0ccf6669014486869c38ce2
daon <126737580+shin-daon@users.noreply.github.com>
2023-08-21 17:15:42 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.45 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        
+        Arrays.sort(numbers);
+        
+        int result1 = numbers[0] * numbers[1];
+        int result2 = numbers[numbers.length - 1] * numbers[numbers.length - 2];
+        
+        if(result1 > result2){
+            return result1;
+        } else {
+            return result2;
+        }
+
+    }
+}
