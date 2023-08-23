b2de4304cb285e9455f6a824bef4032679224421
daon <126737580+shin-daon@users.noreply.github.com>
2023-08-23 21:27:01 +0900

    [level 0] Title: 합성수 찾기, Time: 0.04 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n) {
+        int result = 0;
+        int count;
+        
+        for(int i=1; i<=n; i++){
+            count = 0;
+            for(int j=1; j<=i; j++){
+                if(i % j == 0){
+                    count++;
+                }
+            }
+            if(count >= 3){
+                result++;
+            }
+        }
+        
+        return result;
+    }
+}
