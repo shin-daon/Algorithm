a5ea855c34495bf7267aafca9035019f1e99e3b7
daon <126737580+shin-daon@users.noreply.github.com>
2024-10-03 15:15:42 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 1.67 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        String[] arr = my_string.split("");
+        
+        for(int i=0; i<arr.length; i++){
+            answer += arr[i].repeat(n);
+        }
+        
+        return answer;
+    }
+}
