5f563bd4d1bb5d95c3e0724791fab7c8964d7472
daon <126737580+shin-daon@users.noreply.github.com>
2024-10-05 22:27:48 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.84 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string) {
+        my_string = my_string.toLowerCase();
+        
+        String[] arr = my_string.split("");
+        Arrays.sort(arr);
+        
+        String answer = String.join("", arr);
+        
+        return answer;
+        
+    }
+}
